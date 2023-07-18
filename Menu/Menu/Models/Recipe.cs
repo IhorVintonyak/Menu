a4d760656c63b5ed8fe5d7ad2f3853bcaa14b05f
IhorVintonyak <66689738+IhorVintonyak@users.noreply.github.com>
2023-07-18 22:26:26 +0200

    Form
    Changed project to ASP.NET Core Web with MVC control. And created a form to send information.

---
+++ Menu/Menu/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace Menu.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int TempoPreparazione { get; set; }
+
+        public string[] Ingredienti { get; set; }
+
+
+    }
+}
