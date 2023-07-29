beb4d0e7fff1455151dddb6cc1012bf11085ec2b
IhorVintonyak <66689738+IhorVintonyak@users.noreply.github.com>
2023-07-29 21:29:03 +0200

    MVP design
    Changed design and grid system. Made MVP design

---
+++ Menu/Menu/Class/Recipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu.Class
+{
+    internal class Recipe
+    {
+        private string name;
+        private string description;
+        private string[] ingredients;
+        private int preparationTime;
+
+        public Recipe()
+        {
+            this.Name = "Unknown";
+            this.Description = "Unknown";
+            this.PreparationTime = 0;
+        }
+
+        public Recipe(string name, string description, string[] ingredients, int preparationTime)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Ingredients = ingredients;
+            this.PreparationTime = preparationTime;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public string[] Ingredients { get => ingredients; set => ingredients = value; }
+        public int PreparationTime { get => preparationTime; set => preparationTime = value; }
+
+
+    }
+}
---
+++ Menu/Menu/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Menu.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Menu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            string recipeName = "Carbonara";
+            string recipeDescription = "Traditional Italian dish";
+            string[] recipeIngredients = { "Spaghetti", "Tuorli ", "Guanciale", "Pecorino romano", "Pepe nero" };
+            int recipeTime = 15;
+
+            Recipe newRecipe = new Recipe(recipeName, recipeDescription, recipeIngredients, recipeTime);
+
+
+            var bc = new BrushConverter();
+            spRecipe.Background = (Brush)bc.ConvertFrom("#FFFFFF");
+            tbName.Text = newRecipe.Name;
+            tbDescription.Text = newRecipe.Description;
+            tbIngredients.Text = newRecipe.Ingredients[0];
+            tbTime.Text = newRecipe.PreparationTime.ToString();
+        }
+    }
+}
